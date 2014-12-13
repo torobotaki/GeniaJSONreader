7bb31abef16a734bd653dd52afbd3c7a64ff74e6
Dialekti Valsamou <dvalsamou@gmail.com>
2014-12-13 01:06:27 +0100

    readme + syntax

---
+++ README.md
@@ -0,0 +1,41 @@
+# Genia JSON reader
+
+This little script is a utility to read the JSON files from the Genia dataset
+for the BioNLP 2013 ST relation extraction task. 
+
+This is my first ever use of python, and I will understand if you feel I write
+non-python python code.
+
+## Installation
+
+see the source code and your OS/python doc
+
+## Usage
+
+Call the program with the directories containing the json files as parameters.
+
+
+## Contributing
+
+In the extremely improbable chance...
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+This project was started on 12/12/2014. That's it
+
+## Credits
+
+This was created in the context of my (Dialekti VALSAMOU) PhD work. I am affiliated to two
+laboratories:
+* MIG - INRA
+* LIMSI - CNRS
+
+## License
+
+Apache 2.0
--- src/readJSON.py
+++ src/readJSON.py
@@ -7,49 +7,48 @@ from pprint import pprint
 import sys
 
 
-def readJSONfile (filename):
-    json_data=open(filename).read()
+def readJSONfile(filename):
+    json_data = open(filename).read()
     data = json.loads(json_data)
     return data
 
+
 def readJSONfiles(args):
-    data=[]
-    for i in range (1,len(args)):
+    data = []
+    for i in range(1, len(args)):
         directory = args[i]
-        for filename in listdir(directory) :
+        for filename in listdir(directory):
             pathFilename = join(directory, filename)
             if isfile(pathFilename):
-                if  pathFilename.endswith(".json"):
+                if pathFilename.endswith(".json"):
                     data.append(readJSONfile(pathFilename))
     return data
 
-# TODO: add arg types
 
 def getTypes(alldata):
-    entityTypes ={}
+    entityTypes = {}
     relationTypes = {}
-    sections = {}
     for data in alldata:
-        datasection = parseJSONdata(data);
-        sections[datasection.name] = datasection;
+        datasection = parseJSONdata(data)
         for etypename in datasection.entityTypes:
             enttype = datasection.entityTypes[etypename]
-            if etypename not in entityTypes: entityTypes[enttype] = 1;
-            else: entityTypes[enttype] += 1
+            if etypename not in entityTypes:
+                entityTypes[enttype] = 1
+            else:
+                entityTypes[enttype] += 1
         for rtypename in datasection.relationTypes:
             reltype = datasection.relationTypes[rtypename]
-            if rtypename not in relationTypes: 
-                relationTypes[rtypename] = reltype;
-            else: 
+            if rtypename not in relationTypes:
+                relationTypes[rtypename] = reltype
+            else:
                 relationTypes[rtypename].occurrences += reltype.occurrences
                 for atype in reltype.arg1types:
                     relationTypes[rtypename].addarg1type(atype, reltype.arg1types[atype])
                 for atype in reltype.arg2types:
-                    relationTypes[rtypename].addarg2type(atype,reltype.arg2types[atype])
-    return entityTypes, relationTypes, sections
+                    relationTypes[rtypename].addarg2type(atype, reltype.arg2types[atype])
+    return entityTypes, relationTypes
 
 
-                
 def parseJSONdata(data):
     sectionName = data["section"]
     source_db = data["source_db"]
@@ -62,18 +61,19 @@ def parseJSONdata(data):
             span = dataentity["span"]
             start = span["begin"]
             end = span["end"]
-            newentity = entity(eid, start, end, etype);
-            newsection.entities[eid] =newentity;
-            if etype not in newsection.entityTypes: newsection.entityTypes[etype] = etype
+            newentity = entity(eid, start, end, etype)
+            newsection.entities[eid] = newentity
+            if etype not in newsection.entityTypes:
+                newsection.entityTypes[etype] = etype
     if 'insanns' in data:
         for instance in data["insanns"]:
             tid = instance["id"]
             eid = instance["object"]
-            newtrigger = eventTrigger(tid, newsection.entities[eid]); 
-            newsection.triggers[tid] = newtrigger;
+            newtrigger = eventTrigger(tid, newsection.entities[eid])
+            newsection.triggers[tid] = newtrigger
     if 'relanns' in data:
         for datarelation in data["relanns"]:
-            rid = datarelation["id"];
+            rid = datarelation["id"]
             rtype = unun(datarelation["type"])
             robject = datarelation["object"]
             rsubject = datarelation["subject"]
@@ -83,21 +83,14 @@ def parseJSONdata(data):
                 newreltype = relationType(rtype)
                 newreltype.addarg1type(newsection.getentitytype(rsubject))
                 newreltype.addarg2type(newsection.getentitytype(robject))
-                newreltype.occurrences = 1;
+                newreltype.occurrences = 1
                 newsection.relationTypes[rtype] = newreltype
             else:
                 newsection.relationTypes[rtype].addarg1type(newsection.getentitytype(rsubject))
                 newsection.relationTypes[rtype].addarg2type(newsection.getentitytype(robject))
-                newsection.relationTypes[rtype].occurrences +=1;
+                newsection.relationTypes[rtype].occurrences += 1
     return newsection
-                
 
-class entityType:
-    def __init__(self,name):
-        self.name = name
-    def name(self):
-        return self.name
-    
 
 class entity:
     def __init__(self, eid, charStart, charEnd, etype):
@@ -106,18 +99,21 @@ class entity:
         self.eid = eid
         self.etype = etype
 
+
 class eventTrigger:
     def __init__(self, tid, entity):
         self.triggerid = tid
         self.entity = entity
-        
+
+
 class relation:
     def __init__(self, rid, robject, rsubject, rtype):
         self.rid = rid
         self.robject = robject
         self.rsubject = rsubject
         self.rtype = rtype
-        
+
+
 class section:
     def __init__(self, name, sourcedb, sourceid):
         self.name = name
@@ -128,59 +124,68 @@ class section:
         self.triggers = {}
         self.entityTypes = {}
         self.relationTypes = {}
+
     def getentitytype(self, eid):
         if eid in self.entities:
             return self.entities[eid].etype
         elif eid in self.triggers:
             return "event"
 
+
 class relationType:
     def __init__(self, name):
         self.name = name
         self.arg1types = {}
         self.arg2types = {}
-        self.occurrences = 0;
+        self.occurrences = 0
+
     def name(self):
         return self.name
+
     def addarg1type(self, argtype, occ=1):
         if argtype not in self.arg1types:
-            self.arg1types[argtype] = occ;
+            self.arg1types[argtype] = occ
         else:
-            self.arg1types[argtype] += occ;
+            self.arg1types[argtype] += occ
+
     def addarg2type(self, argtype, occ=1):
         if argtype not in self.arg2types:
-            self.arg2types[argtype] = occ;
+            self.arg2types[argtype] = occ
         else:
-            self.arg2types[argtype]+= occ;
+            self.arg2types[argtype] += occ
+
     def args(self, arg1, arg2):
         self.addarg1type(arg1)
         self.addarg2type(arg2)
-    def argwords(self,arg1, arg2):
+
+    def argwords(self, arg1, arg2):
         self.addarg1typeword(arg1)
         self.addarg2typeword(arg2)
+
     def getargTypes(self):
         return self.arg1types, self.arg2types
-        
+
 
 def unun(thing):
-    newthing = thing;
+    newthing = thing
     if isinstance(thing, unicode):
             newthing = thing.encode('utf8')
     return newthing
 
+
 def printrelationdetails(relationTypes):
     for reltypename in relationTypes:
         reltype = relationTypes[reltypename]
-        print reltypename, reltype.occurrences;
+        print reltypename, reltype.occurrences
         print "\tsubject types \t"
         for atype in reltype.arg1types:
-            print "\t\t"+atype+":",reltype.arg1types[atype]
+            print "\t\t " + atype + ":", reltype.arg1types[atype]
         print "\tobject types \t"
         for atype in reltype.arg2types:
-            print "\t\t"+atype+":",reltype.arg2types[atype]
-        
+            print "\t\t" + atype + ":", reltype.arg2types[atype]
+
 
 data = readJSONfiles(sys.argv)
-entityTypes, relationTypes, section = getTypes(data)
+entityTypes, relationTypes = getTypes(data)
 pprint(entityTypes)
 printrelationdetails(relationTypes)
